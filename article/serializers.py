af715c5ad61af3282f9c648f2c7d4d1fb186b404
Jihye An <koanko.dev@gmail.com>
2023-07-06 13:15:32 +0900

    refactor: refac article serializer to show comment author

--- article/serializers.py
+++ article/serializers.py
@@ -4,36 +4,13 @@ from django.contrib.auth import get_user_model
 from .models import Article, Comment
 from accounts.models import Clinic
 
-class ArticleSerializer(serializers.ModelSerializer):
-    class UserSerializer(serializers.ModelSerializer):
-        class ClinicSerializer(serializers.ModelSerializer):
-             class Meta:
-                model = Clinic
-                fields = ('id', 'clinic_name', 'address_area',)
-        clinic_info = ClinicSerializer(read_only=True, many=True)
 
-        class Meta:
-            model = get_user_model()
-            fields = ('id', 'username', 'clinic_info',)
-
-    class CommentSerializer(serializers.ModelSerializer):
-        class Meta:
-                model = Comment
-                fields = ('id', 'user', 'content',)
-    comments = CommentSerializer(read_only=True, many=True)
-
-    user = UserSerializer(read_only=True)
-                      
-    class Meta:
-        model = Article
-        fields = ('id', 'user', 'title', 'content', 'comments',)
-
-class CommentSerializer(serializers.ModelSerializer):
-    class UserSerializer(serializers.ModelSerializer):
+class UserSerializer(serializers.ModelSerializer):
         class Meta:
             model = get_user_model()
             fields = ('id', 'username',)
-    
+
+class CommentSerializer(serializers.ModelSerializer):
     class ArticleSerializer(serializers.ModelSerializer):    
         class Meta:
             model = Article
@@ -45,3 +22,29 @@ class CommentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Comment
         fields = ('id', 'article', 'user', 'content',)
+
+
+class ArticleSerializer(serializers.ModelSerializer):
+    class UserClinicSerializer(serializers.ModelSerializer):
+        class ClinicSerializer(serializers.ModelSerializer):
+             class Meta:
+                model = Clinic
+                fields = ('id', 'clinic_name', 'address_area',)
+        clinic_info = ClinicSerializer(read_only=True, many=True)
+
+        class Meta:
+            model = get_user_model()
+            fields = ('id', 'username', 'clinic_info',)
+
+    class CommentSerializer(serializers.ModelSerializer):
+        user = UserSerializer(read_only=True)
+        class Meta:
+                model = Comment
+                fields = ('id', 'user', 'content',)
+
+    comments = CommentSerializer(read_only=True, many=True)
+    user = UserClinicSerializer(read_only=True)
+                      
+    class Meta:
+        model = Article
+        fields = ('id', 'user', 'title', 'content', 'comments',)
